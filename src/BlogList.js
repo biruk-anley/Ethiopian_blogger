e1d0d3122cf0a6fd8a9a45ab29c4d92664cb0997
biruk <birukanley2022@gmail.com>
2022-04-18 16:23:00 +0300

    Add bloglist props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,17 @@
-const BlogList = ({ blogse, title,handleDelete }) => {
-    return (
-        <div className="blog-list">
-            <h2>{title}</h2>
-            
-          {blogse.map((blog) => (
-          <div className="blog-preview" key={blog.id} >
-            <h2>{blog.title}</h2>
-              <p>የ ተጻፈው በ {blog.author}</p>
-              <button onClick={()=>handleDelete(blog.id)}>Delete</button>
-          </div>
-          ))}
-       </div>
-    );
-}
- 
+const BlogList = ({ blogse, title, handleDelete }) => {
+  return (
+    <div className="blog-list">
+      <h2>{title}</h2>
+
+      {blogse.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>የ ተጻፈው በ {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
