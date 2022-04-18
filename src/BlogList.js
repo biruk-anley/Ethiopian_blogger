b0493282c0b158460bc4ba726007bb721c229f31
biruk <birukanley2022@gmail.com>
2022-04-18 16:20:47 +0300

    Add content for filter method

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,16 @@
-const BlogList = ({ blogse, title }) => {
+const BlogList = ({ blogse, title,handleDelete }) => {
     return (
         <div className="blog-list">
             <h2>{title}</h2>
             
-        {blogse.map((blog) => (
-          <div className="blog-preview">
+          {blogse.map((blog) => (
+          <div className="blog-preview" key={blog.id} >
             <h2>{blog.title}</h2>
-            <p>written by {blog.author}</p>
+              <p>የ ተጻፈው በ {blog.author}</p>
+              <button onClick={()=>handleDelete(blog.id)}>Delete</button>
           </div>
-        ))}
-      </div>
+          ))}
+       </div>
     );
 }
  
