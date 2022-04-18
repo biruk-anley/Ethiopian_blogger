ed3bd71884805cd3bebf0f8c955bb36982a61de8
biruk <birukanley2022@gmail.com>
2022-04-18 12:56:40 +0300

    building up the root component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+
+import Navabr from "./Navbar";
+import Home from "./Home";
+// import SideBar from "./SideBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navabr />
+      {/* <SideBar /> */}
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
-
+ 
 export default App;
