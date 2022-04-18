50bcaedd6ab94ce4a7943d8a7c1b12fa66ba7e88
biruk <birukanley2022@gmail.com>
2022-04-18 13:59:18 +0300

    Adding an output list

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 const Home = () => {
-    const shoot = (n,e) => {
-        console.log("clicked " + n, e.buttons);
-    }
+    const [blogs, setBlogs] = useState([
+      {
+        title: "Wulibta",
+        body: "lorem lorem lorem epseum so that no onwe",
+        author: "Alemayehu Gelagay",
+        id: 1,
+      },
+      {
+        title: "Lijnet",
+        body: "fungay ke hitsanu gar eyetechawete eylal mushira hun tebale",
+        author: "Zenebe wella",
+        id: 2,
+      },
+      {
+        title: "Abocher",
+        body: "salsedbachew eskezare semche emalakewun sidib sedebugn ",
+        author: "Endalegeta Kebede",
+        id: 3,
+      },
+    ]);
+   
     return ( 
         <div className="home">
-            <h2>Home page</h2>
-            <button onClick={(e)=>{shoot("100000", e)}}>click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview">
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+            )
+            )}   
         </div>
      );
 }
