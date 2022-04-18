6aa0728b8780ecfd7ad593194aaa6f6afe79652e
biruk <birukanley2022@gmail.com>
2022-04-18 15:07:47 +0300

    adding the 4th title for showing bloglist

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,43 @@
 import { useState } from "react";
+import BlogList from "./BLogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-      {
-        title: "Wulibta",
-        body: "lorem lorem lorem epseum so that no onwe",
-        author: "Alemayehu Gelagay",
-        id: 1,
-      },
-      {
-        title: "Lijnet",
-        body: "fungay ke hitsanu gar eyetechawete eylal mushira hun tebale",
-        author: "Zenebe wella",
-        id: 2,
-      },
-      {
-        title: "Abocher",
-        body: "salsedbachew eskezare semche emalakewun sidib sedebugn ",
-        author: "Endalegeta Kebede",
-        id: 3,
-      },
-    ]);
-   
-    return ( 
-        <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview">
-                    <h2>{blog.title}</h2>
-                    <p>written by {blog.author}</p>
-                </div>
-            )
-            )}   
-        </div>
-     );
-}
-  
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Wulibta",
+      body: "lorem lorem lorem epseum so that no onwe",
+      author: "Alemayehu Gelagay",
+      id: 1,
+    },
+    {
+      title: "Lijnet",
+      body: "fungay ke hitsanu gar eyetechawete eylal mushira hun tebale",
+      author: "Zenebe wella",
+      id: 2,
+    },
+    {
+      title: "Abocher",
+      body: "salsedbachew eskezare semche emalakewun sidib sedebugn ",
+      author: "Endalegeta Kebede",
+      id: 3,
+    },
+    {
+      title: "Wurisa",
+      body: "lorem lorem amperopir lorem epseum so that no onwe",
+      author: "Alemayehu Gelagay",
+      id: 4,
+    },
+  ]);
+
+  return (
+    <div className="home">
+      <BlogList blogse={blogs} title="All of the Blogs" />
+      <BlogList
+        blogse={blogs.filter((blog) => blog.author === "Alemayehu Gelagay")}
+        title="Alemayehu blog"
+      />
+    </div>
+  );
+};
+
 export default Home;
