1efe1baf65ef6e4bfe7de6264e5e05ce193f7179
biruk <birukanley2022@gmail.com>
2022-04-19 14:34:14 +0300

    Adding routing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@
 
 import Navabr from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Create from "./Create";
 // import SideBar from "./SideBar";
 
 function App() {
   return (
-    <div className="App">
-      <Navabr />
-      {/* <SideBar /> */}
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navabr />
+        {/* <SideBar /> */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
- 
+
 export default App;
