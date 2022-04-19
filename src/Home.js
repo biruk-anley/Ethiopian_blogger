251f6b65b3b294670efdf264d7ba75526717b839
biruk <birukanley2022@gmail.com>
2022-04-19 14:35:32 +0300

    updating  home

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,12 @@
-import { useState, useEffect } from "react";
 import BlogLists from "./BlogLists";
+import UseFetch from "./UseFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isloading, setIsloading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // this is the fucnction which is going to be renderd at the first time and  ever time the state chnage there will be rendering
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs") //get response object 2 use json method and tackle data
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("couldn't fetch data since it doesn't exist"); // this is unable to fetch data error :: to see this error chang feth url like blogss
-          }
-          return res.json(); // this will parse json into js object
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsloading(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setError(err.message); // this is network error handler // to see this type of error disconnect json server::
-          setIsloading(false); // to remove loading message
-        });
-    }, 2000);
-  }, []);
+  const {
+    data: blogs,
+    isloading,
+    error,
+  } = UseFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import blog2 from "./blog2.png";
 const Navabr = () => {
   return (
@@ -5,21 +6,11 @@ const Navabr = () => {
       <img src={blog2} className="imgs" alt="img logo" />
       <h2>Ethiopian's Blog</h2>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-              color: "white",
-              backgroundColor: "#353bf1b4",
-              borderRadius:"8px"
-            
-          }}
-        >
-          New Blog
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/create"> New Blog</Link>
       </div>
     </nav>
   );
-};
+}; 
 
 export default Navabr;
