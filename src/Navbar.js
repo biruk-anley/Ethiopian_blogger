887b4bd960829b330c7628c9cf9e0e0c1bcb2684
biruk <birukanley2022@gmail.com>
2022-04-19 14:38:28 +0300

    Add style for navabar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,4 @@
+
 import { Link } from "react-router-dom";
 import blog2 from "./blog2.png";
 const Navabr = () => {
@@ -7,10 +8,20 @@ const Navabr = () => {
       <h2>Ethiopian's Blog</h2>
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/create"> New Blog</Link>
+        <Link
+          to="/create"
+          style={{
+              color: "white",
+              backgroundColor: "#353bf1b4",
+              borderRadius:"8px"
+            
+          }}
+        >
+          New Blog
+        </Link>
       </div>
     </nav>
   );
-}; 
+};
 
 export default Navabr;
