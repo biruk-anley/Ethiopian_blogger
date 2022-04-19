5577f5b1f8971bdda8c0d45ae2d8341f453af2ad
biruk <birukanley2022@gmail.com>
2022-04-19 16:24:48 +0300

    Updating blog list with link routing page

--- src/BlogLists.js
+++ src/BlogLists.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogLists = ({ blogse, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogse.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>የ ተጻፈው በ {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>የ ተጻፈው በ {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
